efc9cf74f469cffdfc55cc433cbd6ce120c6e0b4
Phil Huynh <philiphuynh98@gmail.com>
2023-05-07 22:38:55 -0400

    adding big numbers

--- javascript/addingBigNumbers.js
+++ javascript/addingBigNumbers.js
@@ -1,3 +1,19 @@
+/*
+We need to sum big numbers and we require your help.
+
+Write a function that returns the sum of two numbers. The input numbers are strings and the function must return a string.
+
+Example
+add("123", "321"); -> "444"
+add("11", "99");   -> "110"
+Notes
+The input numbers are big.
+The input is a string of only digits
+The numbers are positives
+
+*/
+
+
 function add(a, b) {
   a = a.split("")
   b = b.split("")
