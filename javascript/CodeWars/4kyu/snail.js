17aba9b690e65d6373212d710975f27e80ea012c
Phil Huynh <philiphuynh98@gmail.com>
2023-05-17 00:58:55 -0400

    snail js

--- javascript/CodeWars/4kyu/snail.js
+++ javascript/CodeWars/4kyu/snail.js
@@ -1,4 +1,6 @@
 /*
+https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/train/javascript
+
 Snail Sort
 Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
 
