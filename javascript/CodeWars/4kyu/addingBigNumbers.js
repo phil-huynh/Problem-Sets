b68ef1d0d44a0e55067b466151f58c5e09418b23
Phil Huynh <philiphuynh98@gmail.com>
2023-06-16 02:37:06 -0400

    consonant values js

--- javascript/CodeWars/4kyu/addingBigNumbers.js
+++ javascript/CodeWars/4kyu/addingBigNumbers.js
@@ -15,48 +15,18 @@ The numbers are positives
 
 */
 
-
 function add(a, b) {
-  a = a.split("")
-  b = b.split("")
-  a.reverse()
-  b.reverse()
-  let carried = 0
-  let result = []
-  let top;
-  let bottom;
-  if (a.length >= b.length) {
-    top = a
-    bottom = b
-  } else {
-    top = b
-    bottom = a
-  }
-  top.forEach((number, index) => {
-    let total = 0
-    if(carried == 1) {
-      total = 1
-    }
-    if (bottom[index]) {
-     total += (Number(number) + Number(bottom[index]))
-    } else {
-      total += Number(number)
-    }
-    if (total >= 10) {
-      carried = 1
-      total = total.toString()
-      total = Number(total[1])
-    } else {
-      carried = 0
-    }
-    result.push(total)
-    total = 0
-    if (index === top.length - 1){
-      if (carried === 1) {
-        result.push(1)
-      }
-    }
+  a = a.split("").reverse();
+  b = b.split("").reverse();
+  let carried = 0, result = []
+  const [top, bottom] = a.length >= b.length ? [a, b] : [b, a]
+
+  top.forEach((num, i) => {
+    let total = carried;
+    total += bottom[i] ? Number(num) + Number(bottom[i]) : Number(num);
+    carried = total >= 10 ? 1 : 0;
+    carried ? result.push(Number(total.toString()[1])) : result.push(total);
+    (i === top.length - 1) && carried ? result.push(1) : null;
   })
-  result.reverse()
-  return result.join("")
+  return result.reverse().join("");
 }
